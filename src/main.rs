6af4b9c1c062b113009bcf73e15616946b6e2ec7
dddddjent <1306043330a@gmail.com>
2023-11-22 11:18:37 +0800

    Log

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use log::{info, LevelFilter};
+use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
+
 use self::configuration::Configuration;
 use self::data_processor::direct::DirectDataProcessor;
 use self::data_processor::DataProcessor;
@@ -13,6 +16,15 @@ mod util;
 mod world;
 
 fn main() {
+    TermLogger::init(
+        LevelFilter::Info,
+        Config::default(),
+        TerminalMode::Mixed,
+        ColorChoice::Auto,
+    )
+    .unwrap();
+    info!("Start!");
+
     let args: Vec<String> = std::env::args().collect();
     let config_path = Path::new(&args[1][..]).join("config.json");
     let configuration = Configuration::new(config_path.to_str().unwrap());
@@ -21,8 +33,9 @@ fn main() {
     if let Some(output_path) = configuration.process_only_output_path {
         let output_path = config_path.parent().unwrap().join(output_path);
         DirectDataProcessor::new().write(output_path.to_str().unwrap(), &world);
+        info!("Finished successfully!");
         return;
     }
 
-    println!("Finished successfully!");
+    info!("Finished successfully!");
 }
